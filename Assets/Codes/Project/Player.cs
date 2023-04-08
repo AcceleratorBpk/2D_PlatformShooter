9ef7b0fb074382f6932b04e85f3f0285aa6aa8b5
Bpk <lixiang98bpk@outlook.com>
2023-04-09 04:14:21 +0800

    实现基础3C

---
+++ Assets/Codes/Project/Player.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+namespace PlatformShoot
+{
+    public class Player : MonoBehaviour
+    {
+        private Rigidbody2D mRig;
+        private float mGroundMoveSpeed = 5f;
+        private float mJumpForce = 12f;
+        private void Start()
+        {
+            mRig = GetComponent<Rigidbody2D>();
+        }
+        private void FixedUpdate()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Debug.Log("¥•∑¢¡À");
+                mRig.velocity = new Vector2(mRig.velocity.x, mJumpForce);
+            }
+
+            mRig.velocity = new Vector2(Input.GetAxisRaw("Horizontal")* mGroundMoveSpeed, mRig.velocity.y);
+        }
+    }
+}
