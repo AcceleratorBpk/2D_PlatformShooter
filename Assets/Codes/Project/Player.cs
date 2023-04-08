1b38f2ad1e571ef5592067fe990bad6b07ee8bc4
Bpk <lixiang98bpk@outlook.com>
2023-04-09 05:33:46 +0800

    基本触发游戏管理器

---
+++ Assets/Codes/Project/Player.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace PlatformShoot
+{
+    public class Player : MonoBehaviour
+    {
+        private Rigidbody2D mRig;
+        private float mGroundMoveSpeed = 5f;
+        private float mJumpForce = 12f;
+        private void Start()
+        {
+            mRig = GetComponent<Rigidbody2D>();
+        }
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.J))
+            {
+             var bullet=Resources.Load<GameObject>("Bullet");
+                GameObject.Instantiate(bullet, transform.position, Quaternion.identity);
+            }
+        }
+        private void FixedUpdate()
+        {
+            if (Input.GetKeyDown(KeyCode.K))
+            {
+                Debug.Log("¥•∑¢¡À");
+                mRig.velocity = new Vector2(mRig.velocity.x, mJumpForce);
+  
+            }
+
+            mRig.velocity = new Vector2(Input.GetAxisRaw("Horizontal")* mGroundMoveSpeed, mRig.velocity.y);
+        }
+        private void OnTriggerEnter(Collider coll)
+        {
+            if (coll.gameObject.CompareTag("Door"))
+            {
+                SceneManager.LoadScene("GamePassScene");
+            }
+        }
+    }
+}
